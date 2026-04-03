9b81cda970cae0bcb500c7ccc223576f4f496a67
Vasu Sidapara <74949446+SidaparaVasu@users.noreply.github.com>
2026-04-03 18:39:52 +0530

    Feature/backend/vasu Implement module 'Training Need Identification' (#8)
    * feat: add global base repository for data abstraction
    
    * feat: add global base service for business orchestration
    
    * refactor: implement repository layer for skill_management
    
    * refactor: align skill_management services with clean architecture
    
    * refactor: skill_management and rbac app to distinction between 'exports' and 'logic'
    
    * refactor: move RBAC business logic from views to services
    
    * refactor: implement standard architecture to org_management
    
    * Initialize TNI Management app and Add Models
    
    * Add Repository Services for TNI Module
    
    * Implement logic engine for automated Training Needs Identification
    
    * Implement serializers and view sets (API layer)
    
    * Add Django Admin Controls and Migration file
    
    * Update the SkillGapSnapshot model to make current_level nullable
    
    * Corrected an invalid ORM filter
    
    attempting to check is_active on EmployeeMaster (which uses employment_status instead)
    
    * Add Test Suite for TNI Services, Logic Engine, APIs

---
+++ backend/apps/tni_management/repositories/__init__.py
@@ -0,0 +1,18 @@
+from .tni_repository import (
+    TrainingNeedRepository,
+    SkillGapSnapshotRepository,
+    ComplianceRequirementRepository,
+    TNIApprovalRepository,
+    TNICourseRecommendationRepository,
+    TNIAnalysisRepository
+)
+
+
+__all__ = [
+    "TrainingNeedRepository",
+    "SkillGapSnapshotRepository",
+    "ComplianceRequirementRepository",
+    "TNIApprovalRepository",
+    "TNICourseRecommendationRepository",
+    "TNIAnalysisRepository"
+]
